cf0d732efc70fb8ea08f0143f371dfcbc257e7da
alegarcro <alegarcro@myuax.com>
2024-10-15 11:34:38 +0200

    Ejercio 1

---
+++ main2.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Alex Garcia on 15/10/2024.
+//
+
+#include "main2.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo" << std::endl;
+    return 0;
+}
---
+++ main2.h
@@ -0,0 +1,21 @@
+//
+// Created by Alex Garcia on 15/10/2024.
+//
+
+#ifndef MAIN2_H
+#define MAIN2_H
+
+
+
+class main2 {
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo" << std::endl;
+    return 0;
+}
+};
+
+
+
+#endif //MAIN2_H
